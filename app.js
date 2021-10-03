fa3b6eab7ff15b6e1715e3678bce1f87de3a2d2e
Muhammad Ahsan <ahsansoftengineer@gmail.com>
2021-10-03 09:13:04 +0500

    Use full Switch Statement

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-a = console.log
-a("Working");
-a('See it is working');
+l = console.log;
+l('Application Started');
+command = process.argv;
+l(command)
+command.forEach((cmd) => {
+  if (
+    cmd === command[0] || cmd === 'D:\\2 Node\\Node\\app.js') l('skipped options');
+  else {
+    switch (cmd) {
+      case '-a':
+      case 'add':
+      case '-c':
+      case 'create':
+        l('ADD / CREATE');
+        break;
+      case '-u':
+      case 'update':
+      case '-m':
+      case 'modified':
+        l('UPDATE / MODIFIED');
+        break;
+      case '-d':
+      case 'del':
+      case 'delete':
+        l('DEL / DELETE');
+        break;
+      case '-g':
+      case 'get':
+        l('GET');
+        break;
+      case '-ga':
+      case 'getAll':
+        l('GET ALL');
+        break;
+      default:
+        l('Option not defined');
+        break;
+    }
+  }
+});
